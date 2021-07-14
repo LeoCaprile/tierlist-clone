5c912468a40b990a056a1c3c7096e4b409feaed7
Matías Fuentes <matiasf.cortez@gmail.com>
2021-07-14 19:39:18 -0400

    Drag And Drop feature

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log("Hola")
+const draggables = document.querySelectorAll('.draggable');
+const imageContainer = document.querySelectorAll('.image-container');
+const imageSelector = document.querySelector('.image-selector')
+draggables.forEach(draggables => {
+    draggables.addEventListener('dragstart', () =>{
+        draggables.classList.add('dragging');
+    })
+
+    draggables.addEventListener('dragend',()=>{
+        draggables.classList.remove('dragging');
+    })
+    
+})
+imageContainer.forEach(containers =>{
+    containers.addEventListener('dragover',()=>{
+        const draggable = document.querySelector('.dragging');
+        containers.appendChild(draggable);
+    })
+})
+
+imageSelector.addEventListener('dragover', ()=>{
+    const draggable = document.querySelector('.dragging');
+        imageSelector.appendChild(draggable);
+})

ff8de79b35357ba8d0240695d59666502870dcf6
Matías Fuentes <matiasf.cortez@gmail.com>
2021-07-17 20:17:10 -0400

    Upload images and display them in app. Feature added

--- main.js
+++ main.js
@@ -1,24 +1,70 @@
-const draggables = document.querySelectorAll('.draggable');
-const imageContainer = document.querySelectorAll('.image-container');
-const imageSelector = document.querySelector('.image-selector')
-draggables.forEach(draggables => {
-    draggables.addEventListener('dragstart', () =>{
-        draggables.classList.add('dragging');
-    })
+const file = document.querySelector(".file-upload");
 
-    draggables.addEventListener('dragend',()=>{
-        draggables.classList.remove('dragging');
+
+//DRAG AND DROP FEATURE
+function updateDraggables(){
+    const draggables = document.querySelectorAll('.draggable');
+    const imageContainer = document.querySelectorAll('.image-container');
+    const imageSelector = document.querySelector('.image-selector');
+
+    draggables.forEach(draggables => {
+        draggables.addEventListener('dragstart', () =>{
+            draggables.classList.add('dragging');
+        })
+    
+        draggables.addEventListener('dragend',()=>{
+            draggables.classList.remove('dragging');
+        })
+        
+    })
+    imageContainer.forEach(containers =>{
+        containers.addEventListener('dragover',()=>{
+            const draggable = document.querySelector('.dragging');
+            containers.appendChild(draggable);
+        })
     })
     
-})
-imageContainer.forEach(containers =>{
-    containers.addEventListener('dragover',()=>{
+    imageSelector.addEventListener('dragover', ()=>{
         const draggable = document.querySelector('.dragging');
-        containers.appendChild(draggable);
+            imageSelector.appendChild(draggable);
     })
-})
+}
 
-imageSelector.addEventListener('dragover', ()=>{
-    const draggable = document.querySelector('.dragging');
-        imageSelector.appendChild(draggable);
+
+//IMAGE UPLOAD TROUGH API
+file.addEventListener('change',(e)=>{
+
+const formData = new FormData();
+formData.append("image",e.target.files[0]);
+
+fetch("https://api.imgur.com/3/image", {
+	"method": "POST",
+	"headers": {
+		"Authorization": "Client-ID 5e51192d9242f14"
+	},
+	"body":formData,
+}).then(res => res.json())
+.then(data =>{
+
+    if(data.success === true){
+        console.log(data)
+        const imageSelector = document.querySelector(".image-selector");
+        const img = document.createElement('div');
+
+        img.setAttribute("style", `background-image: url(${data.data.link});background-size: cover; height: 80px; width: 80px;`)
+        img.setAttribute('class','draggable')
+        img.setAttribute('draggable','true')
+
+        imageSelector.appendChild(img)
+        updateDraggables();
+        }
+        else{
+            console.log(data)
+        }
+        
+    }
+    )
+    .catch(err=> console.log(err))
+
+
 })
